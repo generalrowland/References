3bb0f839379b12a78f686db8db458e393b77fbb6
generalrowland <113939475+generalrowland@users.noreply.github.com>
2023-08-14 15:19:48 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using References;
+
+Sedan myFirstSedan = new Sedan(0);
+IAutomobile myAutomobile = myFirstSedan;
+
+Sedan myOtherSedan = new Sedan(0);
+
+myFirstSedan.IncreaseSpeed();
+Console.WriteLine(myFirstSedan.Speed);
+Console.WriteLine(myAutomobile.Speed);
+Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+
+myOtherSedan.IncreaseSpeed();
+Console.WriteLine(myFirstSedan.Speed);
+Console.WriteLine(myOtherSedan.Speed);
+Console.WriteLine(myFirstSedan.Equals(myAutomobile));
+
+Truck myTruck = new Truck(50, 500, "MyTruck");
+
+// Calls stringify method and describes automobiles.
+myFirstSedan.Stringify();
+myAutomobile.Stringify();
+myOtherSedan.Stringify();
+myTruck.Stringify();
--- Sedan.cs
+++ Sedan.cs
@@ -8,15 +8,31 @@ namespace References
 {
     class Sedan : IAutomobile
     {
-        public double Speed => { get; private set; };
+        public double Speed  { get; private set; }
 
-        public int Wheels => { get; private set; };
+        public int Wheels  { get; private set; }
+        
+        public string LicensePlate  { get; private set; }
 
-        public string LicensePlate => { get; private set; };
+        public Sedan(double speed)
+        {
+            Wheels = 4;
+            Speed = speed;
+            LicensePlate = "GCTC-06";
+        }
 
         public void Stringify()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"The Sedan is traveling at a speed of {Speed} on {Wheels} wheels, with a Liscense Plat # of {LicensePlate}.");
+        }
+        public void IncreaseSpeed()
+        {
+            Speed += 5;
+        }
+
+        public void DecreaseSpeed()
+        {
+            Speed -= 5;
         }
     }
 }
